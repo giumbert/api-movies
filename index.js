1a9a32a53020f98925f465fe6b41ab577e5f8512
giumbert <humbertojzapatag@gmail.com>
2021-10-01 16:27:11 -0300

    Added modulo for read .env files

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+require('dotenv').config({path: '.env'});
+
 app.use(morgan('combined'));
 
 require('./routes/routes')(app);
 require('./startup/db')();
 require('./startup/validation')();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
