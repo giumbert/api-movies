d734bb2399ed81186f4a45035195c2b6cc96ceea
giumbert <humbertojzapatag@gmail.com>
2021-10-01 13:04:27 -0300

    Added morgan package

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('combined'));
 
 require('./routes/routes')(app);
 require('./startup/db')();
@@ -7,12 +10,6 @@ require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  // winston
-  //   .add(new winston.transports.Console())
-  //   .log({
-  //     level: 'info',
-  //     message: `Listening on port ${port}...!`
-  //   });
   console.log(`Listening on port: ${port}`);
 });
 
